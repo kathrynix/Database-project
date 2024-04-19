840c6e8cc0b901e63a782024aa79b61236576fcc
kathrynix <kathrynix61@gmail.com>
2024-04-19 16:49:34 -0400

    Added keys and more tables to the schema

--- Finalprojectschema.sql
+++ Finalprojectschema.sql
@@ -3,6 +3,9 @@ drop table customer cascade constraints;
 drop table item cascade constraints;
 drop table sale cascade constraints;
 drop table saledescription cascade constraints;
+drop table purchases cascade constraints;
+drop table itemsbought cascade constraints;
+drop table processedby cascade constraints;
 
 create table employee (
     employeeid int not null,
@@ -12,7 +15,9 @@ create table employee (
     address varchar(100),
     startdate date,
     enddate date,
-    hourlysalary number(5, 2)
+    hourlysalary number(5, 2),
+    bankaccount int,
+    constraint employeepk primary key(employeeid)
 );
 
 create table customer (
@@ -20,21 +25,50 @@ create table customer (
     firstname varchar(20),
     lastname varchar(20),
     phonenumber int,
-    address varchar(100)
+    address varchar(100),
+    creditcard int,
+    constraint customerpk primary key(customerid)
 );
 
 create table item (
     itemid int not null,
     itemname varchar(100),
-    price number(6, 2)
+    price number(6, 2),
+    constraint itempk primary key(itemid)
 );
 
-create table sale (
+create table purchases (
     transactionid int not null,
-    discountid int not null
+    customerid int not null,
+    constraint purchasespk primary key(transactionid),
+    constraint purchasesfk foreign key(customerid) references customer(customerid)
 );
 
 create table saledescription (
     discountid int not null,
-    description varchar(250)
+    description varchar(250),
+    constraint saledescpk primary key(discountid)
+);
+
+create table sale (
+    transactionid int not null,
+    discountid int not null,
+    constraint salepk primary key(transactionid, discountid),
+    constraint salefk foreign key(discountid) references saledescription(discountid)
+);
+
+create table itemsbought (
+    transactionid int not null,
+    itemid int not null,
+    constraint ibpk primary key(transactionid, itemid),
+    constraint ibfk1 foreign key(transactionid) references purchases(transactionid),
+    constraint ibfk2 foreign key(itemid) references item(itemid)
+);
+
+create table processedby (
+    transactionid int not null,
+    employeeid int not null,
+    constraint pbpk primary key(transactionid),
+    constraint pbfk1 foreign key(transactionid) references purchases(transactionid),
+    constraint pbfk2 foreign key(employeeid) references employee(employeeid)
 );
